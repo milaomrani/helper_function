a750031c41206c5dc97e8e872b87f6171036b4d0
Milad Omrani <50385765+milaomrani@users.noreply.github.com>
2023-01-26 09:40:42 -0500

    Create helper_function.py

---
+++ helper_function.py
@@ -0,0 +1,20 @@
+# How to read a folder of images and crop the images from middle and save two part in png ground truth and source in python
+
+import os
+import cv2
+
+# path of images
+img_path = "/content/drive/MyDrive/dataset/train"
+
+corr_path = "/content/corrupted_images"
+src_path = "/content/source_images"
+
+for filename in os.listdir(img_path):
+  img = cv2.imread(os.path.join(img_path, filename))
+  # crop the image from middle
+  height, width, _ = img.shape
+  croppted_img = img[0:height, 0:width//2]
+  src_img = img[0:height, width//2:width]
+  # save the two parts
+  cv2.imwrite(os.path.join(corr_path, "corr_"+filename), croppted_img)
+  cv2.imwrite(os.path.join(src_path, "src_"+filename), src_img)
